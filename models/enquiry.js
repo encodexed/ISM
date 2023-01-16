2287f62627da697f03940101ad732330eeaee125
Robbie Gollan <robbiegollan@gmail.com>
2023-01-16 12:37:14 +1100

    added enquiry model, updated enrolment page

--- models/enquiry.js
+++ models/enquiry.js
@@ -2,16 +2,23 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const EnquirySchema = new Schema({
-    // parentName: String,
-    // email: String,
-    // contactNumber: String,
-    // studentName: String,
-    // dataOfBirth: String,
-    // gender: {
-    //     type: String,
-    //     enum: ['male', 'female', 'other']
-    // },
-    // notes: String
+    // Parent
+    parentFirstName: String,
+    parentLastName: String,
+    email: String,
+    contactNumber: String,
+    // Student
+    studentFirstName: String,
+    studentLastName: String,
+    dataOfBirth: String,
+    gender: {
+        type: String,
+        enum: ['Male', 'Female', 'Other', 'Unsure']
+    },
+    notes: String,
+    // Music Program
+    desiredMusicProgram: String,
+    preferredTime: String
 })
 
 module.exports = mongoose.model('Enquiry', EnquirySchema);
