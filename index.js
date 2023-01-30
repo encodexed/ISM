c65780268a3009e7c2b916491d7066af40a310c4
Robbie Gollan <robbiegollan@gmail.com>
2023-01-30 19:05:22 +1100

    added a scheduling function for future write-outs

--- index.js
+++ index.js
@@ -11,6 +11,27 @@ const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const session = require('express-session');
 const ExpressError = require('./utils/ExpressError');
+const CronJob = require('cron').CronJob;
+
+const job = new CronJob(
+	'0 15 * * * *',
+    function () {
+        let date_ob = new Date();
+        // adjust 0 before single digit date
+        let date = ("0" + date_ob.getDate()).slice(-2);
+        let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+        let year = date_ob.getFullYear();
+        let hours = date_ob.getHours();
+        let minutes = date_ob.getMinutes();
+        // let seconds = date_ob.getSeconds();
+        console.log(`Ding! The current time is ${hours}:${minutes}, ${date}/${month}/${year}`);
+        console.log(year + "-" + month + "-" + date + " " + hours + ":" + minutes);
+		console.log('You will see this message at :15 of every hour. Perhaps a method could be written to backup your database in this time.');
+	},
+	null,
+	true,
+	'America/Los_Angeles'
+);
 
 mongoose.set('strictQuery', true); // suppresses deprecation warning
 mongoose.connect(`mongodb://localhost:27017/ism-leichhardt`, {
